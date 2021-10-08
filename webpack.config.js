4a22b5eccff7a32a9a966a71b3e464c75fda53c2
lxg <479161553@qq.com>
2021-10-08 21:39:37 +0800

    v2

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: 'development',
+  entry: "./src/index.js",
+  output: {
+    filename: "index.[contenthash].js"
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "aaa",
+      template: "src/assets/index.html"
+    })
+  ],
+  module: {
+    rules: [
+    
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"]
+      },
+  
+    ],
+  },
+};
